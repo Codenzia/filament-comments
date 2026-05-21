98f2567c6f07397749d3a5929fe7f7bd920a4756
Mohammad Elhaj <mh2x@hotmail.com>
2026-05-21 21:19:14 +0300

    ci(tests): register Blade Icons + Filament Support providers so view components resolve

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Codenzia\FilamentComments\Tests;
 
+use BladeUI\Icons\BladeIconsServiceProvider;
 use Codenzia\FilamentComments\FilamentCommentsServiceProvider;
+use Filament\Support\SupportServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -10,13 +12,16 @@ class TestCase extends Orchestra
 {
     /**
      * Livewire is listed explicitly because its "livewire.finder" binding
-     * doesn't survive Testbench's package:discover alone. Filament's own
-     * providers + Blade/Icons providers are auto-discovered.
+     * doesn't survive Testbench's package:discover alone. Blade Icons +
+     * Filament Support are also listed so tests that render
+     * <x-filament::icon> in views can resolve the view component.
      */
     protected function getPackageProviders($app): array
     {
         return [
+            BladeIconsServiceProvider::class,
             LivewireServiceProvider::class,
+            SupportServiceProvider::class,
             FilamentCommentsServiceProvider::class,
         ];
     }
